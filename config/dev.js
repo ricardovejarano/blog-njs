e1693ef197dcd0c88967d335f2aeb49f070353fc
Ricardo <ricardo.vejarano.garcia@gmail.com>
2020-06-24 18:49:27 -0500

    Blog for node js

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '726055505746-sb6l09a6sns7jenpv80sjam5cbsjqjqn.apps.googleusercontent.com',
+  googleClientSecret: '8GQnJ_p8SVqvoQr1vTxSMFzJ',
+  mongoURI: 'mongodb://p00gz:BD0bOzpCooN1lTvX@cluster0-shard-00-00-hcxqb.mongodb.net:27017,cluster0-shard-00-01-hcxqb.mongodb.net:27017,cluster0-shard-00-02-hcxqb.mongodb.net:27017/blog_dev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
---
+++ tests/header.test.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
+
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await browser.close();
+})
+
+test('the header has the correct test', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts 0auth flow', async () => {
+    await page.waitFor('.right a');
+    await page.click('.right a');
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+    // const id = '5eeea58d2841994b3c2851fd';
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+
+    await page.setCookie({ name: 'session', value: session  });
+    await page.setCookie({ name: 'session.sig', value: sig  });
+
+    await page.goto('localhost:3000');
+
+    await page.waitFor('a[href="/auth/logout"]');
+
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+    expect(text).toEqual('Logout');
+});
